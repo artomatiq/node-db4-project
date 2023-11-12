e91499343f664adf258c7ac9270602df4b77f14a
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-12 10:46:54 -0500

    wip transfer

--- data/migrations/20231110201934_initial-migration.js
+++ data/migrations/20231110201934_initial-migration.js
@@ -5,13 +5,16 @@
 exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', table => {
-            table.increments()
+            table.increments('recipe_id')
+            table.string('recipe_name', 200).notNullable().unique()
         })
         .createTable('steps',  table => {
-            table.increments()
+            table.increments('step_id')
         })
         .createTable('ingredients', table => {
-            table.increments()
+            table.increments('ingredient_id')
+            table.string('ingreient_name', 200).notNullable().unique()
+            table.string('ingredient_unit')
         })
         .createTable('step_ingredients', table => {
             table.increments()
