5c23346811d4f9133bb55f0c76fd7950e37e38a9
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-13 17:03:58 -0500

    different approach

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -4,7 +4,6 @@ async function getSteps(recipe_id) {
     const steps = await db('steps')
         .select('step_id', 'step_number', 'step_instructions')
         .where('recipe_id', recipe_id)
-    
     return steps
 }
 
@@ -12,12 +11,10 @@ async function getIngredients(step_id) {
     const ingredient_ids = await db('step_ingredients')
         .select('ingredient_id')
         .where('step_id', step_id)
-
     const ingredients = await db('ingredients as i')
         .select('i.ingredient_id', 'ingredient_name', 'quantity', 'ingredient_unit')
         .join('step_ingredients as si', 'i.ingredient_id', 'si.ingredient_id')
         .whereIn('i.ingredient_id', ingredient_ids.map(row=>row.ingredient_id))
-
     return ingredients
 }
 
@@ -26,13 +23,12 @@ async function getById (recipe_id) {
         .select('*')
         .where('recipe_id', recipe_id)
         .first()
-
+    recipe.created_at = new Date().toUTCString()
     recipe.steps = await getSteps(recipe_id)
-
     for (const step of recipe.steps) {
         step.ingredients = await getIngredients(step.step_id)
     }
-    console.log(recipe)
+    return recipe
 }
 
 module.exports = {getById, getSteps, getIngredients}
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,13 +4,11 @@ const Recipe = require('./recipes-model')
 const router = express.Router()
 
 router.get('/:recipe_id', (req, res, next) => {
-    // Recipe.getById(req.params.recipe_id)
-    //     .then (recipe => {
-    //         res.status(200).json(recipe)
-    //     })
-    //     .catch(next)
-
-        Recipe.getById(req.params.recipe_id)
+    Recipe.getById(req.params.recipe_id)
+        .then (recipe => {
+            res.status(200).json(recipe)
+        })
+        .catch(next)
 })
 
 module.exports = router
