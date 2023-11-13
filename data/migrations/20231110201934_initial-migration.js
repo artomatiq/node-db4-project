4202a65a9a1f2c6f4c9fb92e2a4877106efcda16
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-12 22:32:16 -0500

    seed

--- data/migrations/20231110201934_initial-migration.js
+++ data/migrations/20231110201934_initial-migration.js
@@ -1,3 +1,5 @@
+const { unsubscribe } = require("../../api/server");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -10,14 +12,40 @@ exports.up = async function(knex) {
         })
         .createTable('steps',  table => {
             table.increments('step_id')
+            table.string('step_text', 200).notNullable()
+            table.integer('step_number').notNullable().unsigned()
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
         })
         .createTable('ingredients', table => {
             table.increments('ingredient_id')
-            table.string('ingreient_name', 200).notNullable().unique()
+            table.string('ingredient_name', 200).notNullable().unique()
             table.string('ingredient_unit')
         })
         .createTable('step_ingredients', table => {
             table.increments()
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            table.integer('quantity')
+                .unsigned()
+
         })
 };
 
